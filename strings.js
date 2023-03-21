d70b89c62f494897239605943d92419ab261ff54
elena tertytsia <tertytsia24@gmail.com>
2023-03-21 11:56:29 +0100

    first steps

---
+++ strings.js
@@ -0,0 +1,29 @@
+const greet = "Hello Students, Welcome to the second week";
+console.log(greet);
+
+console.log(greet.replace("second", "2nd"));
+console.log(greet);
+
+const students = "Students";
+const index = greet.indexOf(students);
+console.log("The index of the word students is " + index);
+
+console.log(greet.toUpperCase);
+
+const concat = "Hello" + " " + "Students";
+console.log(concat);
+
+console.log(`The length of the string is ${greet.length}`);
+
+let viewers = 10;
+console.log((viewers += 1));
+
+const text = students.toString();
+
+const pizza = {
+    name: "Banana Pineapple",
+    slices: 6,
+    isVeg: false,
+};
+
+const pizzas = ["Kebab", "Banana Curry", "Hawaiian"];
